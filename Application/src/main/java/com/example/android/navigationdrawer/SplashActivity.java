ecce7a80eeda72ec318080ed9c8cd98a16086773
MinJu Kim <kimminju_99@naver.com>
2016-09-08 20:36:10 +0900

    #2 Update Memory Project

---
+++ Application/src/main/java/com/example/android/navigationdrawer/SplashActivity.java
@@ -0,0 +1,23 @@
+package com.example.android.navigationdrawer;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by appcreator26 on 2016. 9. 6..
+ */
+public class SplashActivity extends Activity{
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        try{
+            Thread.sleep(4000);
+        }catch(InterruptedException e){
+            e.printStackTrace();
+        }
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+}
